c8b25f12360aa4c610170cd281e7d451c335b466
Gideon <gideon@Mac-1s-MacBook-Pro.local>
2020-04-08 10:32:00 -0400

    updating and deleting posts/ and retrieving comments in each post

--- index.js
+++ index.js
@@ -55,9 +55,65 @@ server.get('/api/posts/:id', (req, res) => {
       })
 })
 
+//UPDATE
+server.put('/api/posts/:id', (req, res) => {
+    const { title, contents } = req.body;
+     // or this:  const { title, contents } = req.body; "count" below is the number of item updated
+    const { id } = req.params;
+    db.update(id, {title, contents}) // the second could be an object(json) or data in an object/json that we want to update 
+      .then(count => { 
+        if (count) {
+            db.findById(id)
+              .then(post => res.status(200).json(post))
+              .catch(error => {
+                  res.status(500).json({ error: "The post information could not be modified." })
+              })
+        } else if(!title || !contents ){
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+      })
+})
 
 
+//DELETE
+server.delete('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.remove(id)
+      .then((count => {
+          if(count){
+              res.status(200).json({ message: "post deleted"})
+          } else {
+              res.status(404).json({ message: "The post with the specified ID does not exist." })
+          }
+      }))
+      .catch(error => {
+          res.status(500).json({ error: "The post could not be removed" })
+      })
+      
+})
 
+server.get('/api/posts/:id/comments', (req, res) => {
+    const { id } = req.params;
+    db.findPostComments(id)
+      .then(comments => {
+          if(comments) {
+              res.status(200).json(comments)
+          } else {
+              res.status(404).json({ message: "The post with the specified ID does not exist." })
+          }
+      })
+      .catch(error => {
+          res.status(500).json({ error: "The comments information could not be retrieved." })
+      })
+})
+
+server.post('/api/posts/:id/comments', (req, res) => {
+    const post = req.body;
+    db.insertComment(post)
+      .then()
+})
 
 
 
